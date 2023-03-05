a95c73542ad0d313f1b4e501fea2729e76cbc48e
Will Freeman <will.freeman@hillmangroup.com>
2023-03-04 23:52:48 -0600

    update article getter

--- python/api.py
+++ python/api.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-#import my_module # TODO: update this to be your python file
 from keywordSearch1 import searchScholarForKeywords
 
 app = Flask(__name__)
@@ -7,7 +6,8 @@ app = Flask(__name__)
 @app.route('/articles')
 def get_articles():
   keywords = request.args.get('keywords')
-  articles = searchScholarForKeywords(keywords, 0) # TODO: update this to be your function
+  index = request.args.get('index')
+  articles = searchScholarForKeywords(keywords, int(index) if index != None else 0)
   return articles
 
 if __name__ == '__main__':
